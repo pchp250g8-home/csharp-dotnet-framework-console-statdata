4963b35b60598773bae293b7aad14ce1f9ee1d18
pchp250g8-home <pchp250g8@tutamail.com>
2025-09-28 22:12:40 +0300

    Added Ability Launch form batch file

---
+++ StartVSCode.cmd
@@ -0,0 +1,7 @@
+@echo off
+set VSCODEPATH=%ProgramFiles%\Microsoft VS Code
+rem set JAVA_HOME=%ProgramFiles%\Java\jdk-24
+rem set PHP_ROOT=%SystemDrive%\phprem 
+rem set PATH=%PHP_ROOT%;%PATH%
+set PATH=%VSCODEPATH%;%PATH%
+start "" Code .
